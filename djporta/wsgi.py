da250d3b368f3400d32d47b49061eab45599a3c1
jualjiman <jualjiman@gmail.com>
2014-06-12 23:29:25 -0400

    desde el server wsgi

--- djporta/wsgi.py
+++ djporta/wsgi.py
@@ -5,25 +5,27 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
 
-# import os
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djporta.settings")
-
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
 
 import os
-import sys
-import site
-
-#site.addsitedir('/home/ec2-user/.virtualenvs/mysite-main/lib/python2.7/site-packages')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djporta.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-path = '/var/zpanel/hostdata/zadmin/public_html/jualjiman_com/djporta/'
+"""
 
-if path not in sys.path:
-    sys.path.append(path)
+import os, sys
+sys.path.append('/var/www/djporta')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djporta.settings'
+
+import django.core.handlers.wsgi
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+def application(environ, start_response):
+	environ['PATH_INFO'] = environ['SCRIPT_NAME'] + environ['PATH_INFO']
+	
+	if environ['wsgi.url_scheme'] == 'https':
+		environ['HTTPS'] = 'on'
+	
+	return _application(environ, start_response)
