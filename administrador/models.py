22c96f0494e10a457ffc16f77478fa340d8f0c2f
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-06-17 23:11:42 -0500

    ....2

--- administrador/models.py
+++ administrador/models.py
@@ -1,6 +1,6 @@
  # -*- coding: utf-8 -*-
 from django.db import models
-#from sorl.thumbnail import ThumbnailField
+from sorl.thumbnail.fields import ImageField
 
 # Create your models here.
 
@@ -51,8 +51,8 @@ class Proyecto(models.Model):
 	tecnologias = models.CharField(max_length=80)
 	fecha = models.CharField(max_length=50)
 	link = models.URLField()
-	#imagen = ThumbnailField(upload_to='proyectos', size=(694, 418))
-	imagen = models.FileField(upload_to="proyectos")
+	imagen = ImageField(upload_to='proyectos', size=(694, 418))
+	#imagen = models.FileField(upload_to="proyectos")
 
 	def __str__(self):
 		return self.titulo
