3cdc3982d5653aacdce6425e8e2b611684584ca3
juan alberto jimenez angel <jualjiman@gmail.com>
2014-07-19 18:40:34 -0500

    renovando sitio
    renovando template portafolio

---
+++ administrador/static/js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+});
+
+$(document).on("click",".btnnb",function(e){
+	var clss = $(this).attr("class");
+	clsf = clss.substring(6);
+
+	if(clsf == "b1")
+	{
+		$("#estu").hide();
+		$("#proy").hide();
+		$("#info").fadeIn();
+	}
+	else if(clsf == "b2")
+	{
+		$("#info").hide();
+		$("#proy").hide();
+		$("#estu").fadeIn();
+	}
+		else if(clsf == "b3")
+	{
+		$("#info").hide();
+		$("#estu").hide();
+		$("#proy").fadeIn();
+	}
+	console.log("done");
+});
