2183ffe2537d02bf86355733e25f6ab2c6a7647c
jualjiman <jualjiman@gmail.com>
2014-07-21 21:35:39 -0500

    UTF-8 encode working

--- djporta/settings.py
+++ djporta/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
