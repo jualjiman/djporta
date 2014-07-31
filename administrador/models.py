6fab2c7c2ec76649f8aeda0bdb47cbbe8e0dd3f1
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-07-31 08:45:21 -0500

    colocando nuevas especificaciones de modelos y aplicandolos al template correspondiente

--- administrador/models.py
+++ administrador/models.py
@@ -46,8 +46,8 @@ class Estudio(models.Model):
 	descripcion = models.TextField(blank=True)
 	#fecha = models.CharField(max_length=50)
 	fecha = models.DateField(default = date.today)
-	lugar = models.CharField(max_length=50, blank=True)
-	link = models.URLField(blank=True)
+	lugar = models.CharField(max_length=50)
+	LugarLink = models.URLField(blank=True)
 	completado = models.BooleanField(default=False)
 
 	def __unicode__(self): # __str__(self):
