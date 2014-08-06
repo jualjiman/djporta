d3bdd4be94bad5e8776c2c3f00e1b3ef801e7558
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-08-06 13:25:41 -0500

    quitando la descripcion de la experiencia profesional como dato a mostrar

--- administrador/static/js/colsSameHeight-Bootstrap-3.js
+++ administrador/static/js/colsSameHeight-Bootstrap-3.js
@@ -1,11 +1,8 @@
 // media queries sizes?
 var sm = 768, md = 992, lg = 1200;
-var delay1 = 1300;//milisecconds
-var delay2 = 5300;//milisecconds
+var delay = 1300;//milisecconds
 
 var $obj, win;
-var elements = Array();
-var nelements = 0;
 
 $(document).ready(function(){
 
@@ -27,26 +24,12 @@ function setObjects(){
 		obj = objects[i];
 		$obj = $(obj);
 		if($obj.length > 0){
-			if(obj.lastIndexOf("-im") != -1){
-				setTimeout(sameHeight,delay1);
-				setTimeout(sameHeight,delay2);
-			}	
+			if(obj.lastIndexOf("-im") != -1)
+				setTimeout(sameHeight,delay);
 			else
 				sameHeight();
 		}
 	}
-}
-
-function getInfo(){
-	var xs,sm,md,lg;
-	var classes = "";
-	$obj.each( function(){
-		classes = $( this ).attr("class").split(" ");
-		for(var i = 0; i<classes.length;i++){
-			console.log(classes[i])
-		}
-
-	});
 }
 
 function sameHeight(){
@@ -56,7 +39,6 @@ function sameHeight(){
 	if(win > sm){
 		// iterando los objectos
 		// $obj.slice(init,fin).each( function(){
-		getInfo();
 		$obj.each( function(){
 			$( this ).height('initial')
 			cur = $( this ).height();
@@ -66,12 +48,12 @@ function sameHeight(){
 	    var min = (maxHeight*0.9);
 	    var nmin = 0;
 	    $obj.each( function(){ 
-	    	if( min < cur )
+	    	if(min < cur)
 	    		nmin++;
 	    });
 	    $obj.each( function(){ 
 	    	cur = $( this ).height();
-	    	if(cur <= min && nmin > 0){
+	    	if(cur <= min && nmin > 1){
 	    		$(this).height(min);
 	    	}
 	    	else{
@@ -84,4 +66,3 @@ function sameHeight(){
 		$obj.each( function(){ $( this ).height('initial')});
 	}
 }
-
