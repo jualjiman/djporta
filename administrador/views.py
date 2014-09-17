0a126bf7c9612b1a8cb78cb1e8c0e45bf8215285
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-09-17 11:33:14 -0500

    cargando contexto pdf

--- administrador/views.py
+++ administrador/views.py
@@ -95,8 +95,5 @@ class HelloPDFView(PDFTemplateView):
 		return super(HelloPDFView, self).get_context_data(
             		pagesize="A4",
             		title="Hi there!",
-            		infos= infos, 
-            		tecnos=tecnos,
-            		lengs=lengs,
-            		exps=exps,
+            		**kwargs
         	)
